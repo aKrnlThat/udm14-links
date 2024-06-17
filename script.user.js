4eb7f5898a1215a5b620ee8d924d9bffe26a0493
aKrnlThat <realplylastic@proton.me>
2024-06-17 22:04:17 +0800

    chore: prep v1.0.1 release

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UDM 14 Links
 // @namespace    http://tampermonkey.net/
-// @version      2024-06-14
+// @version      2024-06-16
 // @description  Adds udm=14 parameter to all Google links on any page for a cleaner look
 // @author       aKrnlThat and contributors
 // @match        http://*/*
